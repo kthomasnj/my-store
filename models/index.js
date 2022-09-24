b14163a0c5f80311ed9ca81fecb3b17450f1b774
Keith S Thomas <kthomasnj@hotmail.com>
2022-09-24 10:24:21 -0400

    Updating product code with route functionality

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignkey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignkey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+  foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  foreignkey: "tag_id"
+});
 
 module.exports = {
   Product,
